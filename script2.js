84e95f100f7b737d77fa5e718c84f2a7cd201127
duriel666 <59049578+duriel666@users.noreply.github.com>
2021-05-06 13:43:28 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,27 +4,7 @@ let latestValue = 3;
 async function getReader() {
     port = await navigator.serial.requestPort({});
     await port.open({ baudRate: 9600 });
-
     connectButton.innerText = '🔌 Disconnect';
-    //document.querySelector('figure').classList.remove('fadeOut');
-    //document.querySelector('figure').classList.add('bounceIn');
-
-    /*const appendStream = new WritableStream({
-        write(chunk) {
-            lineBuffer += chunk;
-
-            let lines = lineBuffer.split('\n');
-
-            if (lines.length > 1) {
-                lineBuffer = lines.pop();
-                latestValue = parseInt(lines.pop().trim());
-                log.console(latestValue);
-            }
-        }
-    });*/
-
     port.readable
-        /*.pipeThrough(new TextDecoderStream())
-        .pipeTo(appendStream);*/
-        return port.readable.getReader();
+    return port.readable.getReader();
 }
